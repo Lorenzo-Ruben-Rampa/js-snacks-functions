dde67039573a55fc3492a7df8fb59125499f56c2
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2025-01-11 02:15:05 +0100

    es 2 non funzionante

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -2,14 +2,41 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
+// Creo un nuovo array dove metterò le iniziali di ogni parola
+const finalArray = [];
+
 // Dichiara la funzione qui.
 let prelievoIniziali = names.map(function Inizialiprelevate(nome) {
-    const finalArray = [];
     return nome[0]
 });
 
+// Inserisco le iniziali prelevate nel nuovo array
+finalArray.push(prelievoIniziali);
+
 // Invoca la funzione qui e stampa il risultato in console
-let finalArray = [Inizialiprelevate(nome)];
-console.log(prelievoIniziali);
+console.log(finalArray);
+
+// Dichiara la funzione qui.
+function filteredArray(value, index, array) {
+    return array.indexOf(value) === index;
+}
+
+let uniqueArray = finalArray.filter(filteredArray);
+
+// Invoca la funzione qui e stampa il risultato in console
+console.log(uniqueArray);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+
+// Altri tentativi
+
+// let uniqueArray = finalArray.filter(function filteredArray (value, index, array) {
+//     return array.indexOf(value) === index;
+// });
+
+// function removeDuplicates(finalArray) {
+//     return finalArray.filter((item, index) => finalArray.indexOf(item) === index);
+// }
+
+// Rimuovo i duplicati
+// let uniqueArray = removeDuplicates(finalArray);
